93bead4ad421a87ed18308791be5e5e691012e3b
Hammad Nadeem <hammadnadeem145@gmail.com>
2024-02-03 17:27:48 +0500

    wagmi-connectors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,27 @@
-import { useAccount, useConnect } from 'wagmi'
-import { InjectedConnector } from 'wagmi/connectors/injected'
+import { useAccount, useConnect, useDisconnect } from 'wagmi'
 
 export default function Home() {
-  const { address, isConnected } = useAccount()
-  const { connect } = useConnect({
-    connector: new InjectedConnector()
-    })
+  const { address, isConnected, } = useAccount()
+  const { connect, connectors } = useConnect()
+  const { disconnect } = useDisconnect()
+
+  // console.log(connectors)
   return (
     <>
       <div>
-        <button onClick={() => connect()}>Connect</button>
+        <button onClick={() => connect(
+          { connector: connectors && connectors[0] }
+        )}>Connect</button>
+        <button onClick={() => connect(
+          { connector: connectors && connectors[1] }
+        )}>Connect wallet connect</button>
+        <button onClick={() => connect(
+          { connector: connectors && connectors[2] }
+        )}>Connect coinbase</button>
+        <button onClick={() => connect(
+          { connector: connectors && connectors[3] }
+        )}>Connect Metamask</button>
+        <button onClick={() => disconnect()}>Disconnect</button>
         <p>{isConnected && address}</p>
       </div>
     </>
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,13 +1,41 @@
-import { WagmiConfig, createConfig, configureChains, mainnet } from 'wagmi'
-import { publicProvider } from 'wagmi/providers/public'
+import { bsc } from "viem/chains";
+import { WagmiConfig, createConfig, configureChains, mainnet } from "wagmi";
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import { publicProvider } from "wagmi/providers/public";
 
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet],
-  [publicProvider()],
-)
+const { publicClient, webSocketPublicClient } = configureChains(
+  [bsc],
+  [
+    publicProvider(),
+    jsonRpcProvider({
+      rpc: () => ({
+        http: `https://bsc-dataseed1.binance.org/`,
+      }),
+    }),
+  ]
+);
 
 export const config = createConfig({
   autoConnect: true,
   publicClient,
   webSocketPublicClient,
-})
+  connectors: [
+    new InjectedConnector(),
+    new WalletConnectConnector({
+      options: {
+        projectId: "25727eee09560ceaebd6ed8bc629aaa1",
+      },
+    }),
+    new CoinbaseWalletConnector({
+      options: {
+        appName: 'wagmi.sh',
+        jsonRpcUrl: 'https://bsc-dataseed1.binance.org/',
+      },
+    }),
+    new MetaMaskConnector()
+  ],
+});
